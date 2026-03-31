85475bc0bf0a7ef420ee6a81a590e032150e41d3
harsh kadam <150789859+HarshLKadam@users.noreply.github.com>
2026-03-31 11:18:35 +0530

    code pipeline working check

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ print("hello this is check for code pipelinework")
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world !'
 
 if __name__ == '__main__':
     app.run()
