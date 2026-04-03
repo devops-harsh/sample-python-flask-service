d498b97f896f11d3810f808e123f20e342e083c7
harsh kadam <150789859+HarshLKadam@users.noreply.github.com>
2026-04-03 07:31:35 +0530

    typo

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@
 set -e
 docker pull harsh2409/sample-python-flask-app
 
-docker run -d -p 5000:5000 arsh2409/sample-python-flask-app
+docker run -d -p 5000:5000 harsh2409/sample-python-flask-app
 
 echo "hi this is start"
